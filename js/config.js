60c58eec05195dbb28533ca1e21c25d8d622bd58
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2026-07-29 21:41:30 +0700

    Add Supabase connection credentials
    Added Supabase connection credentials to config.js.

--- js/config.js
+++ js/config.js
@@ -1,3 +1,8 @@
+// Supabase Connection Credentials
+export const SUPABASE_URL = "https://eotevqndvmxrtbzjsqyt.supabase.co/rest/v1/";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvdGV2cW5kdm14cnRiempzcXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjM2NDksImV4cCI6MjEwMDg5OTY0OX0.k-2w2PFd_2r0X8WX7UYrL3SdBbfE3CegX4VJNqPlxmc";
+
+// စိစစ်သူ အကောင့်စာရင်း
 export const AUTHORIZED_MODERATORS = [
   { name: "ခွန်ဖန်ဒွဲ့", id: "19" },
   { name: "ခွန်တကံ", id: "21" },
