d27e21a51b3bf78580e838f93140cab6f521b3d8
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2026-07-30 00:52:28 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,3 @@
-// Supabase Credentials
-export const SUPABASE_URL = "https://eotevqndvmxrtbzjsqyt.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvdGV2cW5kdm14cnRiempzcXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjM2NDksImV4cCI6MjEwMDg5OTY0OX0.k-2w2PFd_2r0X8WX7UYrL3SdBbfE3CegX4VJNqPlxmc";
-
-// စိစစ်သူ အကောင့်စာရင်း
 export const AUTHORIZED_MODERATORS = [
-  { name: "ခွန်ဖန်ဒွဲ့", id: "19" },
-  { name: "ခွန်တကံ", id: "21" },
-  { name: "ခွန်ရောန", id: "24" }
+  { name: '7D', id: '87' } // လိုအပ်ပါက Admin အမည်နှင့် ID များကို ဤနေရာတွင် ထည့်ပါ
 ];
