51380c1ffff8597c6ec90ac95fac7f7379a5cbd4
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2026-07-29 22:04:12 +0700

    Modify SUPABASE_URL in config.js
    Updated SUPABASE_URL to remove the '/rest/v1/' endpoint.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Supabase Connection Credentials
-export const SUPABASE_URL = "https://eotevqndvmxrtbzjsqyt.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://eotevqndvmxrtbzjsqyt.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvdGV2cW5kdm14cnRiempzcXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjM2NDksImV4cCI6MjEwMDg5OTY0OX0.k-2w2PFd_2r0X8WX7UYrL3SdBbfE3CegX4VJNqPlxmc";
 
 // စိစစ်သူ အကောင့်စာရင်း
